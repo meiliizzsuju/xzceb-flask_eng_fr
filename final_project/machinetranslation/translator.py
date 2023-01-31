f980a68ce7eea81899fded7b5d9a7cce554ba8e2
Ticha Tin <62775008+meiliizzsuju@users.noreply.github.com>
2023-01-31 15:05:08 +1100

    e2f function added

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,20 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+
+def englishToFrench(englishText):
+    translation = language_translator.translate(
+        text=englishText,
+        model_id='en-es').get_result()
+    jsonResultEnToFR = json.dumps(translation, indent=2, ensure_ascii=False)
+    frenchText = translation['translations'][0]['translation']
+    return frenchText
