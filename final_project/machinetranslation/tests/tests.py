1c914cf525a6f299e37f5c4ec663f7ee19affd72
Ticha Tin <62775008+meiliizzsuju@users.noreply.github.com>
2023-01-31 19:03:35 +1100

    packing module

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,13 +4,15 @@ from translator import french_to_english, english_to_french
 
 class TestFrenchToEnglish(unittest.TestCase): 
     def test1(self):
-        self.assertEqual(french_to_english(''),'Text is null')
         self.assertEqual(french_to_english('Bonjour'),'Hello')
+        self.assertNotEqual(french_to_english(''),'Hello')
+        self.assertEqual(french_to_english(''),'Text is null')
 
 class TestEnglishToFrench(unittest.TestCase): 
     def test1(self):
-        self.assertEqual(english_to_french(''),'Text is null')
         self.assertEqual(english_to_french('Hello'),'Bonjour')
+        self.assertNotEqual(english_to_french(''),'Hello')
+        self.assertEqual(english_to_french(''),'Text is null')
 
 
 unittest.main()
